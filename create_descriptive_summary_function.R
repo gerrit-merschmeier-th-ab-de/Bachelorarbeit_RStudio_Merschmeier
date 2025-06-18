1272a4da9855b1e032369df4906414cdcd664c74
gemersTD <s210837@th-ab.de>
2025-06-18 22:05:11 +0200

    add comment for doing the function grouped_by(Prüfverfahren)

--- create_descriptive_summary_function.R
+++ create_descriptive_summary_function.R
@@ -27,3 +27,37 @@ create_descriptive_summary <- function(df) {
     # Auswählen und anordnen der Spalten
     select(variable, statistic, value)
 }
+
+## Für group_by(Prüfverfahren) beispielsweise
+
+# # Funktion für deskriptive Analyse
+# create_descriptive_summary <- function(df) {
+#   df %>%
+#     group_by(Prüfverfahren) %>%
+#     # Mittelwert, Median, Standardabweichung, Varianz für alle Spalten mit Sekundenangabe
+#     summarise(
+#       across(ends_with("_secs"), # bspl.: where(is.numeric) oder c(Kontakt_Anzahl_Anmeldung, Kontakt_Anzahl_Arztgespräch)
+#              list(
+#                mean = ~mean(., na.rm = TRUE),
+#                median = ~median(., na.rm = TRUE),
+#                sd = ~sd(., na.rm = TRUE),
+#                var = ~var(., na.rm = TRUE)
+#              ),
+#              .names = "{.col}_{.fn}"
+#       ), 
+#       .groups = "drop"
+#     ) %>%
+#     # Umwandeln in das Long-Format
+#     pivot_longer(
+#       cols = -Prüfverfahren,
+#       names_to = "variable_statistic",
+#       values_to = "value"
+#     ) %>%
+#     # Zerlegen der Spalte "variable_statistic" in "statistic" und "variable"
+#     mutate(
+#       statistic = str_extract(variable_statistic, "(mean|median|sd|var)$"),
+#       variable = str_remove(variable_statistic, "_(mean|median|sd|var)$")
+#     ) %>%
+#     # Auswählen und anordnen der Spalten
+#     select(Prüfverfahren, variable, statistic, value)
+# }
